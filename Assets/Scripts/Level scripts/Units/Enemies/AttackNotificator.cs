c96e11b5ed2903c190fc102082e68bad0c15dffb
MansyrevAY <mansyrevarsenij@gmail.com>
2020-02-06 22:57:00 +0300

    new attack trigger started

---
+++ Assets/Scripts/Level scripts/Units/Enemies/AttackNotificator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SquadNavigation))]
+public class AttackNotificator : MonoBehaviour
+{
+    public GameObjRuntimeSetSO allPatrols;
+
+    private bool hasStopped = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Hood"))
+        {
+            TriggerAttack();
+        }
+    }
+
+    /// <summary>
+    /// Stops every patrol in allPatrols
+    /// </summary>
+    private void TriggerAttack()
+    {
+        foreach (GameObject patrol in allPatrols.set)
+        {
+            patrol.GetComponent<AttackNotificator>().StopPatrol();
+        }
+    }
+
+    public void StopPatrol()
+    {
+        if (hasStopped)
+            return;
+
+        hasStopped = true;
+        GetComponent<SquadNavigation>().ReadyForAttack();
+    }
+}
