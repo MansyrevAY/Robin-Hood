3a76fae52e81e690c64fb937fe33ff011ec8b1ca
MansyrevAMY <itachi686@gmail.com>
2020-05-19 18:38:20 +0300

    Fixes
    - добавил кнопку рестарта уровня
    - немного поправил движение гвардов

---
+++ Assets/Scripts/Level scripts/UI/RestartButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartLevel() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+}
