2760ba85b726fc36c82fc503e8bca72b810cd040
Pavelkoshkarev <pashok271996@gmail.com>
2020-06-21 19:13:22 +0300

    Added fog -

---
+++ Assets/Scripts/Level scripts/EnvironmentScripts/AutoHider.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoHider : MonoBehaviour
+{
+
+    public GameObject worldObj;
+    public float desiredDistance = 8;
+    public float maxDistance = 100;
+    public bool doShow = true;
+    public bool doScale = false;
+
+    
+    void Update()
+    {
+        dissapearCheck();
+    }
+
+    private void dissapearCheck()
+    {
+        foreach (Transform child in worldObj.transform)
+        {
+            float distance = Vector3.Distance(child.position, transform.position);
+
+            if(distance < desiredDistance)
+            {
+                if (!doShow)
+                {
+                    if (!child.gameObject.activeInHierarchy)
+                    {
+                        child.gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    if(child.gameObject.activeInHierarchy && !doScale)
+                    {
+                        child.gameObject.SetActive(false);
+                    }
+                    else if(child.gameObject.activeInHierarchy && doScale)
+                    {
+                        child.GetComponent<Scaler>().ToggleScale(false);
+                    }
+                }
+            }
+            else
+            {
+                if (!doShow)
+                {
+                    if (child.gameObject.activeInHierarchy)
+                    {
+                        child.gameObject.SetActive(false);
+                    }
+                }
+                else
+                {
+                    if(distance < maxDistance)
+                    {
+                        if (!child.gameObject.activeInHierarchy && !doScale)
+                        {
+                            child.gameObject.SetActive(true);
+                        }
+                        else if (!child.gameObject.activeInHierarchy && doScale)
+                        {
+                            child.gameObject.SetActive(true);
+                            child.GetComponent<Scaler>().ToggleScale(true);
+                        }
+                    }
+                    else
+                    {
+                        child.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Level scripts/EnvironmentScripts/CloudGen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGen : MonoBehaviour
+{
+    public GameObject Cloud;
+    public float width = 10;
+    public float length = 10;
+    public float height = 10;
+    public float CloudSize = 10;
+        
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                //for (int z = 0; z < height; z++)
+                //{
+                //    GameObject cloudobj = Instantiate(Cloud, new Vector3(transform.position.x + x * CloudSize, transform.position.y + z * CloudSize, transform.position.z + y * CloudSize), Quaternion.identity);
+                //    cloudobj.name = "Cloud " + x + "_" + y + "_" + z;
+                //    cloudobj.transform.SetParent(transform);
+                //}
+
+                GameObject cloudobj = Instantiate(Cloud, new Vector3(transform.position.x + x * CloudSize, transform.position.y, transform.position.z + y * CloudSize), Quaternion.identity);
+                cloudobj.name = "Cloud " + x + "_" + y;
+                cloudobj.transform.SetParent(transform); 
+            }
+        }
+    }
+
+   
+}
