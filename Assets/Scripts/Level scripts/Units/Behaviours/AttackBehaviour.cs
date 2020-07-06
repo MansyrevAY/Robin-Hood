f1069843fdf8dee7f0467e216d4307d65a960145
MansyrevAY <mansyrevarsenij@gmail.com>
2020-07-06 10:52:09 +0300

    Upd

--- Assets/Scripts/Level scripts/Units/Behaviours/AttackBehaviour.cs
+++ Assets/Scripts/Level scripts/Units/Behaviours/AttackBehaviour.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(AnimationScript))]
 [RequireComponent(typeof(MovementBehaviour))]
@@ -44,7 +42,6 @@ public abstract class AttackBehaviour : MonoBehaviour
                 GetNextTarget();
             }
         }
-
     }
 
     /// <summary>
---
+++ Assets/Scripts/Tools/StateMachine/StayingState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tools;
+
+namespace Tools
+{
+    public class StayingState : SceneLinkedSMB<HoodBehaviour>
+    {
+        
+    }
+}
